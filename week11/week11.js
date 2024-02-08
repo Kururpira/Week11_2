a28237b6b3cad6e3235b33c5eb041f2b43ff3c60
Kururpira <kururpira@inbox.ru>
2024-02-08 14:39:09 +0300

    commit

--- week11/week11.js
+++ week11/week11.js
@@ -1,25 +1,58 @@
 
 
-addButton.addEventListener('click', function () {
-	const taskText = taskInput.value;
+const inputBox = document.getElementById("input-box");
+const button = document.querySelector("button");
+const list = document.getElementById("list-container");
+const noTasks = document.getElementById("noTasks");
 
-	
-	const newTask = document.createElement('li');
-	
 
-	newTask.textContent = taskText;
-	
 
-	taskList.append(newTask);
-	
-	
-	taskInput.value = '';
-});
-const taskList = document.getElementById('taskList');
-
-taskList.addEventListener('click', function(evt) {
-if (evt.target.tagName === 'LI') {
-evt.target.classList.toggle('completed'); 
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Добавьте задачу в поле ввода");
+    }else{
+        noTasks.remove();
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.appendChild(li);
+        inputBox.value = '';
+        
+    }
+    saveData();
 }
+list.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
 });
 
+button.addEventListener("click", addTask);
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+    
+}
+function showTask(){
+    const savedData = localStorage.getItem("data");
+    if(savedData){
+        list.innerHTML = savedData;
+        noTasks.remove();
+    }
+}
+document.getElementById('button').onclick = function() {
+    window.localStorage.removeItem('data');
+    list.innerHTML = '';
+    if(list.innerHTML ===''){
+        list.appendChild( noTasks);
+    }
+	else{
+        noTasks.innerHTML = '';
+    }
+
+    
+  }
+window.addEventListener("load", showTask);
